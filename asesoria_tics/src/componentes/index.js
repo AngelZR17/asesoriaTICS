0c9d1b3f9208ba1f991f72cca289ee40e54a8dd5
AngelZR17 <S20120166@alumnos.itsur.edu.mx>
2024-04-23 18:56:54 -0600

    Navbar fija

---
+++ asesoria_tics/src/componentes/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Index = () => {
+    return(
+        <div class = 'divfix'>
+            <h1>¿Qué puedes hacer en la red, internet o wifi?</h1>
+            <p>
+                Esta pagina tiene como objetivo asesorar a los adultos en el uso de las tics
+                ya sea para hacer algun pago de servicios, hacer publicaciones, etc.
+            </p>
+        </div>
+    );
+}
