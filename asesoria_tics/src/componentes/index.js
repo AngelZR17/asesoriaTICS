ecf0aac8b4adf8bce26c346a76d9c4531b559e20
AngelZR17 <S20120166@alumnos.itsur.edu.mx>
2024-04-24 20:40:24 -0600

    problemas con imagen

--- asesoria_tics/src/componentes/index.js
+++ asesoria_tics/src/componentes/index.js
@@ -1,13 +1,20 @@
 import React from "react";
+import imagenEjemplo from '../imagenes/img1.jpg';
 
 export const Index = () => {
     return(
-        <div class = 'divfix'>
+        <div className = 'container'>
             <h1>¿Qué puedes hacer en la red, internet o wifi?</h1>
-            <p>
-                Esta pagina tiene como objetivo asesorar a los adultos en el uso de las tics
-                ya sea para hacer algun pago de servicios, hacer publicaciones, etc.
-            </p>
+            <div className = 'textcontainer'>
+                <p>
+                    Esta pagina tiene como objetivo asesorar a los adultos en el uso de las tics, se les instruira 
+                    para que ellos mismos puedan hacer pagos en linea, usar las redes sociales, aprender a usar ciertas 
+                    aplicaciones como Facebook, Whatsapp, etc.
+                </p>
+                
+            </div>
+            <img className='img1' src={imagenEjemplo}/>
+            
         </div>
     );
 }
